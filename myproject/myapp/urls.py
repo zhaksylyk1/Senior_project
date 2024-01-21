1190cb4da1a6ad1db950ee8e122c2947fa164e24
zhaksylyk1 <zhk27aa@gmail.com>
2024-01-21 21:52:20 +0600

    log2

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,5 +4,6 @@ from myapp.views import my_supabase_view
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
+    path('login2/', views.login_view2, name='login2'),
     path('supabase-data/', my_supabase_view, name='supabase_data'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -20,6 +20,20 @@ def login_view(request):
     else:
         return render(request, 'login.html')
     
+def login_view2(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to a home page
+        else:
+            # Return an 'invalid login' error message
+            return render(request, 'login2.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'login2.html')
+    
 def my_supabase_view(request):
     # Fetch data from Supabase
     data = supabase.table("test_1").select("*").execute()
