1a69c409a91059dfb90fcd13855686bded2b05bc
Batya <ayan.04062002@mail.ru>
2024-01-22 10:25:10 +0600

    Added Authorization connected to supabase, however created new page, didn't used existing login page

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,6 +3,10 @@ from . import views
 from myapp.views import my_supabase_view
 
 urlpatterns = [
+    path('registration_login/', views.register_login_view, name='registration_login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register', views.registerPage, name='register'),
+
     path('login/', views.login_view, name='login'),
     path('login2/', views.login_view2, name='login2'),
     path('supabase-data/', my_supabase_view, name='supabase_data'),
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -6,20 +6,62 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .supabase_client import supabase
 
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+
+
+def register_login_view(request):
+    page = 'registration_login'
+
+    if request.method == "POST":
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request,"User does not exist")
+
+        user = authenticate(request, username = username, password = password)
+
         if user is not None:
             login(request, user)
-            return redirect('home')  # Redirect to a home page
+            return redirect('login')
         else:
-            # Return an 'invalid login' error message
-            return render(request, 'login.html', {'error': 'Invalid username or password'})
-    else:
-        return render(request, 'login.html')
+            messages.error(request,"Username OR password does not exist")
+
+    context = {'page': page}
+    return render(request, 'registration_login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('registration_login')
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('login')
+        else:
+            messages.error(request, "An error ocurred during registration")
+            
+    return render(request, 'registration_login.html', {'form': form})
+
+@login_required(login_url='registration_login')
+def login_view(request):
+   
+    return render(request, 'login.html')
     
+@login_required(login_url='registration_login')    
 def login_view2(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -45,3 +87,5 @@ def my_supabase_view(request):
 
     # Render the template with the data
     return render(request, 'your_template.html', context)
+
+
